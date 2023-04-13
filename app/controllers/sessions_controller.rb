2c3ea1d8b45413faa42d646398577adb1b468f76
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-13 22:49:27 +0900

    Block a user access after a certain number of attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if login_locked?
+      redirect_to login_path, alert: t('.locked')
+      return
+    end
+
     if (user = User.find_by(email: params[:email])) && user.authenticate(params[:password])
       login(user)
       redirect_to surveys_path
     else
-      redirect_to login_path, alert: t('.failed')
+      ahoy.track('Login failed')
+      redirect_to login_path, alert: t('.failed', value: login_attempts)
     end
   end
 
   def destroy
     logout
     redirect_to root_path
+  end
+
+  private
+
+  def login_locked?
+    login_attempts >= 5
+  end
+
+  def login_attempts
+    Ahoy::Event.where_event('Login failed').where(visit_id: current_visit&.id).where('time > ?', 5.minutes.ago).size
   end
 end
