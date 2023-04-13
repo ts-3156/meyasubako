e6d0321b9640066f2134bf39d99b749326191a53
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-14 01:45:26 +0900

    Add config/puma/production.rb

---
+++ config/puma/production.rb
@@ -0,0 +1,8 @@
+environment 'production'
+threads 4, 4
+workers 2
+pidfile "#{Dir.pwd}/tmp/pids/puma.pid"
+state_path "#{Dir.pwd}/tmp/pids/puma.state"
+bind 'unix:///tmp/puma.sock'
+stdout_redirect "#{Dir.pwd}/log/puma.log", "#{Dir.pwd}/log/puma.log", true
+preload_app!
